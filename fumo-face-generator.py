ea56e5fc9ecb03d822e96aad0142acae5d761d96
SartoRiccardo <rikki.sarto@gmail.com>
2024-12-17 02:17:33 +0100

    feat: Added new options to web server

--- fumo-face-generator.py
+++ fumo-face-generator.py
@@ -22,17 +22,20 @@ async def make_face(request: web.Request) -> web.Response:
         mouth_no = int(request.query["mouth"])
         eyecols = request.query["eyecols"].split(",")[:2] if "eyecols" in request.query else None
         outcols = request.query["outcols"].split(",")[:2] if "outcols" in request.query else None
-        heterochromia = "heterochromia" in request.query and request.query["heterochromia"] != "false"
-        diff_clr_outline = "diff_clr_outline" in request.query and request.query["diff_clr_outline"] != "false"
-        file_format = request.query["format"].upper() if "format" in request.query else "DST"
+        heterochromia = request.query.get("heterochromia", "false").lower() != "false"
+        diff_clr_outline = request.query.get("diff_clr_outline", "false").lower() != "false"
+        file_format = request.query.get("format", "DST").upper()
+        fill_no = int(request.query.get("fill_no", "1"))
+        eye2_no = int(request.query.get("eye2_no", str(eye_no)))
     except KeyError as keyerr:
         return web.Response(status=400, text=f"Missing {keyerr}")
     except ValueError as verr:
         return web.Response(status=400, text=f"Invalid value: {verr}")
 
-    filename = f"generated-e{eye_no}l{lash_no}b{brow_no}m{mouth_no}"
+    filename = f"generated-e{eye_no}+{eye2_no}l{lash_no}b{brow_no}m{mouth_no}f{fill_no}"
     fname_attrs = "".join([
-        "H" if heterochromia else ""
+        "H" if heterochromia else "",
+        "Dco" if diff_clr_outline else "",
     ])
     if len(fname_attrs) > 0:
         filename += f"-{fname_attrs}"
@@ -40,7 +43,7 @@ async def make_face(request: web.Request) -> web.Response:
 
     try:
         face = generator.combine_parts(
-            eye_no,
+            (eye_no, eye2_no),
             lash_no,
             brow_no,
             mouth_no,
@@ -49,6 +52,7 @@ async def make_face(request: web.Request) -> web.Response:
             eyecols=eyecols,
             outcols=outcols,
             file_format=file_format,
+            fill_no=fill_no,
         )  # TODO dont block
     except FileNotFoundError as fnferr:
         return web.Response(status=400, text=f"Part not found: {fnferr.filename}")
